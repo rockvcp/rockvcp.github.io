312df70b4efa8d69e3d35baf64289221812662a5
rockvcp <rockvcpineda@gmail.com>
2015-05-11 02:01:31 +0800

    updated porfolio into new version. Portfolio 2.0

---
+++ assets/js/custom.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+	$( ".logo" ).click(function() {
+	  	$( ".nav-menu" ).animate({width: 'toggle'});
+	  	$(".nav-menu").addClass("toggle");
+	  	$(this).toggleClass("clicked");
+	  	$(".direction").toggleClass("clicked");
+	});
+	var navlink = $('body .nav-menu ul li a');
+	$('body .nav-menu ul li a').click(function() {
+	    $(this).addClass("clicked");
+	    $(this).closest('li').siblings().find('a').removeClass("clicked");	   
+
+		if ($('body .nav-menu ul li .about').hasClass('clicked')) {
+			$(this).closest('body').find('#section #about').css('opacity','1').slideDown( "slow" ).addClass("animate");
+			$(this).closest('body').find('#section #works').hide().css('opacity','0').removeClass("animate");
+			$(this).closest('body').find('#section #contact').hide().css('opacity','0').removeClass("animate");
+			//$(this).closest('body').addClass('about').removeClass("works contact");
+		}
+
+		else if ($('body .nav-menu ul li .works').hasClass('clicked')) {
+			$(this).closest('body').find('#section #works').css('opacity','1').slideDown( "slow" ).addClass("animate");
+			$(this).closest('body').find('#section #about').hide().css('opacity','0').removeClass("animate");
+			$(this).closest('body').find('#section #contact').hide().css('opacity','0').removeClass("animate");
+			//$(this).closest('body').addClass('works').removeClass("about contact");
+		}
+
+		else if ($('body .nav-menu ul li .contact').hasClass('clicked')) {
+			$(this).closest('body').find('#section #contact').css('opacity','1').slideDown( "slow" ).addClass("animate");
+			$(this).closest('body').find('#section #about').hide().css('opacity','0').removeClass("animate");
+			$(this).closest('body').find('#section #works').hide().css('opacity','0').removeClass("animate");
+			//$(this).closest('body').addClass('contact').removeClass("about works");
+		}	
+
+		
+
+		
+		/*var i;
+		var arr = "";
+		var parent = $('#works .box .box-inner .sections .row');
+		for (var i = 0; i < 5; i++) {
+			arr += "<span>test</span>";
+		};
+		$(parent).append(arr);*/
+		
+	});  
+});		
+
